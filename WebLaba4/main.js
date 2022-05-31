e35d3d52718097dca03c59a04b74823b64e07fac
yegorprikhodieko <yaga.pri99@mail.ru>
2022-05-31 15:29:08 +0300

    Laba4.2

--- WebLaba4/main.js
+++ WebLaba4/main.js
@@ -1,31 +1,31 @@
-const animeCards = document.querySelector('.animeCards');
+const Cards = document.querySelector('.Cards');
 const newCard = document.createElement('div');
-const animeTittle = document.createElement('p');
-const animeImg = document.createElement('img');
-const animeText = document.createElement('p');
+const Tittle = document.createElement('p');
+const Img = document.createElement('img');
+const PText = document.createElement('p');
 
-const urlAnime = 'https://collectionapi.metmuseum.org/public/collection/v1/objects';
+const urlObj = 'https://collectionapi.metmuseum.org/public/collection/v1/objects';
 
-const delay = (ms) =>{
+/*const delay = (ms) =>{
   return new Promise ((response)=>{
     setTimeout(()=> response(),ms)
   })
-}
-animeTittle.innerHTML = "Museum exhibits: ";
-animeText.innerHTML = "Years: ";
+}*/
+Tittle.innerHTML = "Museum exhibits: ";
+PText.innerHTML = "Years: ";
 
 async function fetchAsyncTodos(){
   try {
-    const response = await fetch(urlAnime);
-    const animeData = await response.json();
+    const response = await fetch(urlObj);
+    const data = await response.json();
     
-      const urlFacts = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${animeData.objectIDs[68]}`;
+      const urlFacts = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${data.objectIDs[68]}`;
       const response2 = await fetch(urlFacts);
       const factsData = await response2.json();
 
-      animeTittle.innerHTML += factsData.objectName;
-      animeImg.src = factsData.primaryImage;
-      animeText.innerHTML+= factsData.accessionYear;
+      Tittle.innerHTML += factsData.objectName;
+      Img.src = factsData.primaryImage;
+      PText.innerHTML+= factsData.accessionYear;
       
       //await delay(3000);
     
@@ -42,14 +42,14 @@ newCard.style.display = 'flex'
 newCard.style.flexDirection = 'column'
 newCard.style.alignItems = 'center'
 newCard.style.justifyContent = 'center'
-animeImg.style.height = '400px'
+Img.style.height = '400px'
 
-animeText.style.maxWidth = '350px'
-animeText.style.textAlign = 'center'
-animeText.style.fontWeight = '20px'
+PText.style.maxWidth = '350px'
+PText.style.textAlign = 'center'
+PText.style.fontWeight = '20px'
 
-newCard.append(animeTittle)
-newCard.append(animeImg)
-newCard.append(animeText)
+newCard.append(Tittle)
+newCard.append(Img)
+newCard.append(PText)
 
-console.log(animeCards.append(newCard))
+console.log(Cards.append(newCard))
